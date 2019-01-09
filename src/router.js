0f3ab6a66d2fc38762cf5df4350b180b7e46d33f
Ivan Silva <isilva@MBP-de-Ivan.lan>
2019-01-09 15:05:12 +0000

    Identation correction

--- src/router.js
+++ src/router.js
@@ -1,71 +1,72 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import Vue from "vue";
+import Router from "vue-router";
 
-import Overview from './views/Overview.vue';
-import UserProfile from './views/UserProfile.vue';
-import Errors from './views/Errors.vue';
-import ComponentsOverview from './views/ComponentsOverview.vue';
-import RegisteredUsers from './views/RegisteredUsers.vue';
-import DatabaseUpdates from './views/DatabaseUpdates.vue';
-import AccessCount from './views/AccessCount.vue';
+import Overview from "./views/Overview.vue";
+import UserProfile from "./views/UserProfile.vue";
+import Errors from "./views/Errors.vue";
+import ComponentsOverview from "./views/ComponentsOverview.vue";
+import RegisteredUsers from "./views/RegisteredUsers.vue";
+import DatabaseUpdates from "./views/DatabaseUpdates.vue";
+import AccessCount from "./views/AccessCount.vue";
 
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  linkActiveClass: 'active',
-  linkExactActiveClass: 'exact-active',
+  linkActiveClass: "active",
+  linkExactActiveClass: "exact-active",
   scrollBehavior() {
     return {
       x: 0,
       y: 0
     };
   },
-  routes: [{
-      path: '/',
+  routes: [
+    {
+      path: "/",
       redirect: {
-        name: 'overview',
-      },
+        name: "overview"
+      }
     },
     {
-      path: '/overview',
-      name: 'overview',
-      component: Overview,
+      path: "/overview",
+      name: "overview",
+      component: Overview
     },
     {
-      path: '/user-profile',
-      name: 'user-profile',
-      component: UserProfile,
+      path: "/user-profile",
+      name: "user-profile",
+      component: UserProfile
     },
     {
-      path: '/500',
-      name: 'errors',
-      component: Errors,
+      path: "/500",
+      name: "errors",
+      component: Errors
     },
     {
-      path: '/components-overview',
-      name: 'components-overview',
-      component: ComponentsOverview,
+      path: "/components-overview",
+      name: "components-overview",
+      component: ComponentsOverview
     },
     {
-      path: '/access-count',
-      name: 'access-count',
-      component: AccessCount,
+      path: "/access-count",
+      name: "access-count",
+      component: AccessCount
     },
     {
-      path: '/registered-users',
-      name: 'registered-users',
-      component: RegisteredUsers,
+      path: "/registered-users",
+      name: "registered-users",
+      component: RegisteredUsers
     },
     {
-      path: '/database-updates',
-      name: 'database-updates',
-      component: DatabaseUpdates,
+      path: "/database-updates",
+      name: "database-updates",
+      component: DatabaseUpdates
     },
     {
-      path: '*',
-      redirect: '/errors',
-    },
-  ],
+      path: "*",
+      redirect: "/errors"
+    }
+  ]
 });
